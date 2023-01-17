ed7c7f93749883b68d546138bf3cacd81ebfa7f0
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-01-17 19:05:14 +0100

    checking if the .tex has been compiled

--- pre_commit_hooks/print_arguments.py
+++ pre_commit_hooks/print_arguments.py
@@ -2,11 +2,15 @@
 #triger ?
 import argparse
 import os
+import datetime
 
 
 def print_arguments(arguments: list[str]):
     for argument in arguments:
         print(argument)
+
+def excpetionThrow(filepath):
+    raise Exception("Hey, it seems like you have not compiled the latest version of " + filepath + " yet, compile it into a pdf before commiting it to the branch")
 
 
 def main():
@@ -15,12 +19,21 @@ def main():
     args = parser.parse_args()
     print(args)
     print("___")
+    timeCheckTex = {}
+    timeCheckPdf = {}
 
-    with open("C:\\Thomas\\CheckHook\\ThomasHookTest.txt", "w") as file:
+    with open("ThomasHookTest.txt", "w") as file:
         file.write("Succes, Now need to find the location\n")
         file.write(os.getcwd()+"\n")
         for i in args.filenames:
-            file.write(str(i)+"\n")
+            print(i)
+            if(i.split(".")[-1].lower() == "tex"):
+                timeCheckTex[i.split("i")[0]] = os.path.getmtime(os.getcwd()+"\\"+i)
+            elif(i.split(".")[-1].lower() == "pdf"):
+                timeCheckPdf[i.split("i")[0]] = os.path.getmtime(os.getcwd()+"\\"+i)
+    for texName in timeCheckTex.keys():
+        if timeCheckTex[texName] - timeCheckPdf.get(texName, 0) > 0:
+            excpetionThrow(texName)
     print_arguments(args.filenames)
 
 
